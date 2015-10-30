47d520ad595058c48953ab83a680325df0a63232
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-10-31 02:25:41 +0900

    Capitalize

--- experiments/scripts/_init_path.py
+++ experiments/scripts/_init_path.py
@@ -6,5 +6,5 @@ import os
 
 _this_dir = os.path.dirname(os.path.abspath(__file__))
 
-data_dir = os.path.join(_this_dir, '../../data')
-logs_dir = os.path.join(_this_dir, '../logs')
+DATA_DIR = os.path.join(_this_dir, '../../data')
+LOGS_DIR = os.path.join(_this_dir, '../logs')
