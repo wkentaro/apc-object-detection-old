287231cf8179f259352c874e14b060ee8300e6c1
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-10-31 14:53:17 +0900

    Add roi_dataset.py

---
+++ experiments/scripts/roi_dataset.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+
+import argparse
+import cPickle as pickle
+import os
+
+import numpy as np
+from skimage import io
+from sklearn.datasets import load_files
+from sklearn.cross_validation import train_test_split
+
+from _init_path import *
+
+
+which_set = 'train'
+
+
+data_dir = os.path.join(DATA_DIR, 'dapc15{0}'.format(which_set))
+files = load_files(data_dir, load_content=False)
+
+
+filenames_for_X = []
+filenames_for_y = []
+for fname in files.filenames:
+    fname = os.path.abspath(fname)
+    if fname.endswith('_mask.jpg'):
+        filenames_for_y.append(fname)
+    else:
+        filenames_for_X.append(fname)
+
+
+def bounding_rect_of_mask(mask):
+    where = np.argwhere(mask)
+    (y_start, x_start), (y_end, x_end) = where.min(0), where.max(0) + 1
+    return y_start, y_end, x_start, x_end
+
+
+y = []
+for fname in filenames_for_y:
+    mask = io.imread(fname, as_grey=True)
+    y_start, y_end, x_start, x_end = bounding_rect_of_mask(mask)
+    y_center, x_center = mask.shape[0] / 2, mask.shape[1] / 2
+    y_start /= y_center
+    y_end /= y_center
+    x_start /= x_center
+    x_end /= x_center
+    y.append([y_start, y_end, x_start, x_end])
+y = np.array(y)
+
+
+pkl_fname = os.path.join(LOGS_DIR, 'roi_y_{0}.pkl'.format(which_set))
+with open(pkl_fname, 'wb') as f:
+    pickle.dump(y, f)
+
+IMG_SHAPE = (89, 133)
+
+X = []
+for fname in filenames_for_X:
+    img = io.imread(fname, as_grey=True)
+    X.append(img.reshape(-1) / 255.)
+X = np.array(X)
+
+
+pkl_fname = os.path.join(LOGS_DIR, 'roi_X_{0}.pkl'.format(which_set))
+with open(pkl_fname, 'wb') as f:
+    pickle.dump(X, f)
