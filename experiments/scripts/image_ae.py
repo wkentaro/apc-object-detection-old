d088e64ab4e97e3019aaabc8739b9089dd23711e
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-02 17:59:10 +0900

    image_ae.py with small image size

--- experiments/scripts/image_ae.py
+++ experiments/scripts/image_ae.py
@@ -34,27 +34,33 @@ xp = cuda.cupy if args.gpu >= 0 else np
 # Fixed params
 batchsize = 10
 n_epoch = 20
-input_img_shape = (89, 133, 3)
+input_img_shape = (44, 66, 3)
 
 # Prepare dataset
 print('loading ROI dataset')
-x_train, y_train = fetch_roidata(which_set='train')
-x_test, y_test = fetch_roidata(which_set='test')
+x_train, _ = fetch_roidata(which_set='train')
+x_test, _ = fetch_roidata(which_set='test')
 N, N_test = len(x_train), len(x_test)
+
+x_train = x_train.astype(np.float32)
+# y_train = y_train.astype(np.float32)
+x_test = x_test.astype(np.float32)
+# y_test = y_test.astype(np.float32)
 
 print('N:', N)
 print('N_test:', N_test)
 
+
 # Prepare multi-layer perceptron model
 model = chainer.FunctionSet(
-    l1=F.Linear(35511, 10000),
-    l2=F.Linear(10000, 2000),
-    l3=F.Linear(2000, 100),
-    l4=F.Linear(100, 20),
-    l5=F.Linear(20, 100),
-    l6=F.Linear(100, 2000),
-    l7=F.Linear(2000, 10000),
-    l8=F.Linear(10000, 35511),
+    l1=F.Linear(8712, 4000),
+    l2=F.Linear(4000, 1000),
+    l3=F.Linear(1000, 200),
+    l4=F.Linear(200, 20),
+    l5=F.Linear(20, 200),
+    l6=F.Linear(200, 1000),
+    l7=F.Linear(1000, 4000),
+    l8=F.Linear(4000, 8712),
 )
 if args.gpu >= 0:
     print('converting model to gpu')
@@ -66,14 +72,14 @@ def forward(x_data, y_data, train=True):
     # Neural net architecture
     x, t = chainer.Variable(x_data), chainer.Variable(y_data)
     h1 = F.dropout(F.relu(model.l1(x)),  train=train)
-    h2 = F.dropout(F.relu(model.l2(h1)), train=train)
-    h3 = F.dropout(F.relu(model.l3(h2)), train=train)
-    h4 = F.dropout(F.relu(model.l4(h3)), train=train)
-    h5 = F.dropout(F.relu(model.l5(h4)), train=train)
-    h6 = F.dropout(F.relu(model.l6(h5)), train=train)
-    h7 = F.dropout(F.relu(model.l7(h6)), train=train)
+    h2 = F.dropout(F.relu(model.l2(h1)),  train=train)
+    h3 = F.dropout(F.relu(model.l3(h2)),  train=train)
+    h4 = F.dropout(F.relu(model.l4(h3)),  train=train)
+    h5 = F.dropout(F.relu(model.l5(h4)),  train=train)
+    h6 = F.dropout(F.relu(model.l6(h5)),  train=train)
+    h7 = F.dropout(F.relu(model.l7(h6)),  train=train)
     y = model.l8(h7)
-    return F.softmax_cross_entropy(y, t), F.accuracy(y, t)
+    return F.mean_squared_error(y, t)
 
 
 # Setup optimizer
@@ -86,43 +92,39 @@ for epoch in six.moves.range(1, n_epoch + 1):
 
     # training
     perm = np.random.permutation(N)
-    sum_accuracy = 0
     sum_loss = 0
     for i in six.moves.range(0, N, batchsize):
         x_batch = xp.asarray(x_train[perm[i:i + batchsize]])
-        y_batch = xp.asarray(y_train[perm[i:i + batchsize]])
+        # y_batch = xp.asarray(y_train[perm[i:i + batchsize]])
+        y_batch = x_batch
 
         optimizer.zero_grads()
-        loss, acc = forward(x_batch, y_batch)
+        loss = forward(x_batch, y_batch)
         loss.backward()
         optimizer.update()
 
         if epoch == 1 and i == 0:
-            with open("graph.dot", "w") as o:
+            with open("../logs/image_ae_graph.dot", "w") as o:
                 o.write(c.build_computational_graph((loss, )).dump())
-            with open("graph.wo_split.dot", "w") as o:
+            with open("../logs/image_ae_graph.wo_split.dot", "w") as o:
                 g = c.build_computational_graph((loss, ),
                                                 remove_split=True)
                 o.write(g.dump())
             print('graph generated')
 
         sum_loss += float(loss.data) * len(y_batch)
-        sum_accuracy += float(acc.data) * len(y_batch)
 
-    print('train mean loss={}, accuracy={}'.format(
-        sum_loss / N, sum_accuracy / N))
+    print('train mean loss={}'.format(sum_loss / N))
 
     # evaluation
-    sum_accuracy = 0
     sum_loss = 0
     for i in six.moves.range(0, N_test, batchsize):
         x_batch = xp.asarray(x_test[i:i + batchsize])
-        y_batch = xp.asarray(y_test[i:i + batchsize])
+        # y_batch = xp.asarray(y_test[i:i + batchsize])
+        y_batch = x_batch
 
-        loss, acc = forward(x_batch, y_batch, train=False)
+        loss = forward(x_batch, y_batch, train=False)
 
         sum_loss += float(loss.data) * len(y_batch)
-        sum_accuracy += float(acc.data) * len(y_batch)
 
-    print('test  mean loss={}, accuracy={}'.format(
-        sum_loss / N_test, sum_accuracy / N_test))
+    print('test  mean loss={}'.format(sum_loss / N_test))
